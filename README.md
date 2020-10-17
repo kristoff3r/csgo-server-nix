fb02eec4d3af5203e79418ab7ab2dc5d550acf5d
Kristoffer Søholm <k.soeholm@gmail.com>
2020-10-17 19:03:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# csgo-server-nix
+
+Nix expressions for hosting a CS:GO server as a NixOS systemd service.
---
+++ nixos/default.nix
@@ -0,0 +1,125 @@
+{ config, lib, pkgs, utils, ... }:
+
+with lib;
+
+let
+  cfg = config.services.csgods;
+  csgods-update = pkgs.writeShellScript "update.sh" ''
+    mkdir -p ${cfg.stateDir}
+    echo "Checking for updates"
+    ${pkgs.steamcmd}/bin/steamcmd <<EOF
+      login anonymous
+      force_install_dir ${cfg.stateDir}
+      app_update 740 validate
+      exit
+    EOF
+    ${pkgs.patchelf}/bin/patchelf \
+       --set-interpreter "$(cat ${pkgs.stdenv_32bit.cc}/nix-support/dynamic-linker-m32)" \
+       --set-rpath "${cfg.stateDir}/bin" \
+       ${cfg.stateDir}/srcds_linux
+    echo "Done updating"
+  '';
+  launchOptions = {
+    tickrate = mkOption {
+      type = types.int;
+      default = 128;
+      description = "Server tickrate";
+    };
+    maxPlayers = mkOption {
+      type = types.int;
+      default = 14;
+      description = "Slots on the server";
+    };
+    port = mkOption {
+      type = types.int;
+      default = 27015;
+      description = "Server port";
+    };
+    map = mkOption {
+      type = types.str;
+      default = "de_dust2";
+      description = "Starting map";
+    };
+    gameLoginToken = mkOption {
+      type = types.str;
+      description = ''
+        Game Login Token, which is required for non-LAN servers.
+        Can be generated on https://steamcommunity.com/dev/managegameservers.
+        Note that you need different ones for each server.
+      '';
+    };
+  };
+in
+{
+  options.services.csgods = {
+    enable = mkEnableOption "CS:GO dedicated server";
+
+    user = mkOption {
+      type = types.str;
+      default = "csgods";
+      description = "User account under which csgods runs.";
+    };
+
+    group = mkOption {
+      type = types.str;
+      default = "csgods";
+      description = "Group under which csgods runs.";
+    };
+
+    stateDir = mkOption {
+      type = types.str;
+      default = "/var/lib/csgods";
+      description = "Directory for CS:GO dedicated server files.";
+    };
+
+    launchOptions = mkOption {
+      type = with types; submodule { options = launchOptions; };
+      default = {};
+      description = "Launch options to be provided for the server";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    users.users = optionalAttrs (cfg.user == "csgods") {
+      csgods = {
+        home = cfg.stateDir;
+        createHome = true;
+        isSystemUser = true;
+        group = cfg.group;
+      };
+    };
+
+    users.groups = optionalAttrs (cfg.group == "csgods") {
+      csgods = { };
+    };
+
+    systemd.services.csgods = {
+      description = "CS:GO dedicated server";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      environment = {
+        LD_LIBRARY_PATH = "${cfg.stateDir}:${cfg.stateDir}/bin";
+      };
+  # default = "-autoupdate -game csgo -usercon -tickrate 128 -port 27015 +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2";
+      serviceConfig = {
+        ExecStartPre = "${csgods-update}";
+        ExecStart = ''${cfg.stateDir}/srcds_run \
+          -game csgo -usercon \
+          +game_type 0 +game_mode 0 \
+          -autoupdate -steam_dir ${cfg.stateDir} -steamcmd_script ${csgods-update} \
+          -tickrate ${toString cfg.launchOptions.tickrate} \
+          -maxplayers ${toString cfg.launchOptions.maxPlayers} \
+          -port ${toString cfg.launchOptions.port} \
+          +map ${cfg.launchOptions.map} \
+          +sv_setsteamaccount ${cfg.launchOptions.gameLoginToken}
+        '';
+        TimeoutStartSec = 0;
+        RestartSec = "120s";
+        Restart = "always";
+        User = cfg.user;
+        Group = cfg.group;
+        WorkingDirectory = "${cfg.stateDir}";
+      };
+    };
+  };
+}
